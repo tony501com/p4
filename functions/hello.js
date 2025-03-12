e4e82e992155ecb3d304c6f8da71d304141a0529
tony501com <tony501@139.com>
2025-03-12 09:57:43 +0800

    更新 Cloudflare Pages

--- functions/hello.js
+++ functions/hello.js
@@ -1,6 +1,6 @@
-// import * as cheerio from 'cheerio';
-const cheerio = require('cheerio');
-  
+import * as cheerio from 'cheerio';
+// import { load } from 'cheerio';
+
 export async function onRequest(context) {
     // 示例 HTML 内容
     const html = `
